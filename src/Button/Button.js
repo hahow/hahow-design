526d24c6ccbe0dabf6d3852df834c7bddb328ff8
Amo Wu <amo260@gmail.com>
2019-12-17 11:56:55 +0800

    fix(Button): 避免樣式被 parent 的 antd.css 覆蓋

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -24,141 +24,140 @@ const buttonType = {
 };
 
 const StyledButton = styled(({ brand, size, type, ...rest }) => <BaseButton {...rest} />)`
-  border-radius: 20px;
-  border: none;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang TC', 'Microsoft JhengHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
-  font-size: 16px;
-  font-weight: 500;
-  height: 40px;
-  line-height: 1.5;
-  width: 180px;
+  &.ant-btn {
+    border-radius: 20px;
+    border: none;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang TC', 'Microsoft JhengHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+    font-size: 16px;
+    font-weight: 500;
+    height: 40px;
+    line-height: 1.5;
+    width: 180px;
 
-  & > .anticon + span {
-    margin-left: 4px;
-  }
 
-  ${variant({
-    prop: 'brand',
-    variants: {
-      [buttonBrand.PRIMARY]: {
-        backgroundColor: 'primary',
-        color: 'white',
-        '&:hover': {
-          backgroundColor: 'primaries.light',
+    ${variant({
+      prop: 'brand',
+      variants: {
+        [buttonBrand.PRIMARY]: {
+          backgroundColor: 'primary',
           color: 'white',
+          '&:hover': {
+            backgroundColor: 'primaries.light',
+            color: 'white',
+          },
+          '&:active, &:focus': {
+            backgroundColor: 'primaries.dark',
+            color: 'white',
+          },
+          '&[disabled], &[disabled]:hover': {
+            backgroundColor: 'blacks.2',
+            borderColor: 'blacks.3',
+            borderStyle: 'solid',
+            borderWidth: '1px',
+          },
         },
-        '&:active, &:focus': {
-          backgroundColor: 'primaries.dark',
+        [buttonBrand.SECONDARY]: {
+          backgroundColor: 'secondary',
           color: 'white',
-        },
-        '&[disabled], &[disabled]:hover': {
-          backgroundColor: 'blacks.2',
-          borderColor: 'blacks.3',
-          borderStyle: 'solid',
-          borderWidth: '1px',
+          '&:hover': {
+            backgroundColor: 'secondaries.light',
+            color: 'white',
+          },
+          '&:active, &:focus': {
+            backgroundColor: 'secondaries.dark',
+            color: 'white',
+          },
         },
       },
-      [buttonBrand.SECONDARY]: {
-        backgroundColor: 'secondary',
-        color: 'white',
-        '&:hover': {
-          backgroundColor: 'secondaries.light',
-          color: 'white',
-        },
-        '&:active, &:focus': {
-          backgroundColor: 'secondaries.dark',
-          color: 'white',
-        },
-      },
-    },
-  })}
-  ${({ brand }) => variant({
-    prop: 'type',
-    variants: {
-      [buttonType.LINK]: {
-        backgroundColor: 'transparent',
-        borderRadius: 0,
-        boxShadow: 'none',
-        color: brand,
-        fontWeight: 600,
-        height: 'auto',
-        padding: 0,
-        width: 'auto',
-        '&:hover': {
+    })}
+    ${({ brand }) => variant({
+      prop: 'type',
+      variants: {
+        [buttonType.LINK]: {
           backgroundColor: 'transparent',
-          color: `${plural(brand)}.light`,
-        },
-        '&:active, &:focus': {
-          backgroundColor: 'transparent',
-          color: `${plural(brand)}.dark`,
-        },
-        '&[disabled], &[disabled]:hover': {
-          backgroundColor: 'transparent',
-          border: 0,
-          color: 'rgba(0, 0, 0, 0.25)',
-        },
-        '&[ant-click-animating-without-extra-node]:after': {
-          opacity: 0,
-        },
-      },
-      [buttonType.PLAIN]: {
-        backgroundColor: '#f5f7f9',
-        color: 'rgba(0, 0, 0, 0.45)',
-        '&:hover': {
-          backgroundColor: '#f5f7f9',
+          borderRadius: 0,
+          boxShadow: 'none',
           color: brand,
+          fontWeight: 600,
+          height: 'auto',
+          padding: 0,
+          width: 'auto',
+          '&:hover': {
+            backgroundColor: 'transparent',
+            color: `${plural(brand)}.light`,
+          },
+          '&:active, &:focus': {
+            backgroundColor: 'transparent',
+            color: `${plural(brand)}.dark`,
+          },
+          '&[disabled], &[disabled]:hover': {
+            backgroundColor: 'transparent',
+            border: 0,
+            color: 'rgba(0, 0, 0, 0.25)',
+          },
+          '&[ant-click-animating-without-extra-node]:after': {
+            opacity: 0,
+          },
         },
-        '&:active, &:focus': {
+        [buttonType.PLAIN]: {
           backgroundColor: '#f5f7f9',
-          color: `${plural(brand)}.dark`,
+          color: 'rgba(0, 0, 0, 0.45)',
+          '&:hover': {
+            backgroundColor: '#f5f7f9',
+            color: brand,
+          },
+          '&:active, &:focus': {
+            backgroundColor: '#f5f7f9',
+            color: `${plural(brand)}.dark`,
+          },
         },
-      },
-      [buttonType.TRANSPARENT]: {
-        backgroundColor: 'transparent',
-        borderColor: brand,
-        borderStyle: 'solid',
-        borderWidth: '2px',
-        color: brand,
-        '&:hover': {
+        [buttonType.TRANSPARENT]: {
           backgroundColor: 'transparent',
-          borderColor: `${plural(brand)}.light`,
-          color: `${plural(brand)}.light`,
+          borderColor: brand,
+          borderStyle: 'solid',
+          borderWidth: '2px',
+          color: brand,
+          '&:hover': {
+            backgroundColor: 'transparent',
+            borderColor: `${plural(brand)}.light`,
+            color: `${plural(brand)}.light`,
+          },
+          '&:active, &:focus': {
+            backgroundColor: 'transparent',
+            borderColor: `${plural(brand)}.dark`,
+            color: `${plural(brand)}.dark`,
+          },
         },
-        '&:active, &:focus': {
-          backgroundColor: 'transparent',
-          borderColor: `${plural(brand)}.dark`,
-          color: `${plural(brand)}.dark`,
-        },
-      },
-      [buttonType.WHITE_THIN]: {
-        backgroundColor: 'white',
-        border: 'solid 1px rgba(0, 0, 0, 0.15)',
-        color: 'rgba(0, 0, 0, 0.45)',
-        '&:hover': {
+        [buttonType.WHITE_THIN]: {
           backgroundColor: 'white',
-          borderColor: `${plural(brand)}.light`,
-          color: `${plural(brand)}.light`,
-        },
-        '&:active, &:focus': {
-          backgroundColor: 'white',
-          borderColor: `${plural(brand)}.dark`,
-          color: `${plural(brand)}.dark`,
+          border: 'solid 1px rgba(0, 0, 0, 0.15)',
+          color: 'rgba(0, 0, 0, 0.45)',
+          '&:hover': {
+            backgroundColor: 'white',
+            borderColor: `${plural(brand)}.light`,
+            color: `${plural(brand)}.light`,
+          },
+          '&:active, &:focus': {
+            backgroundColor: 'white',
+            borderColor: `${plural(brand)}.dark`,
+            color: `${plural(brand)}.dark`,
+          },
         },
       },
-    },
-  })}
-  ${variant({
-    prop: 'size',
-    variants: {
-      [buttonSize.LARGE]: {
-        fontSize: '20px',
-        lineHeight: 1.6,
-        borderRadius: '24px',
-        height: '48px',
-        width: '208px',
+    })}
+    ${variant({
+      prop: 'size',
+      variants: {
+        [buttonSize.LARGE]: {
+          fontSize: '20px',
+          lineHeight: 1.6,
+          borderRadius: '24px',
+          height: '48px',
+          width: '208px',
+        },
       },
-    },
-  })}
+    })}
+  }
 `;
 
 const Button = (props) => {
