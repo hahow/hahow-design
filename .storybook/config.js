1815b94e8efde52d599a34cd35d6648e44da7b91
Amo Wu <amo260@gmail.com>
2019-12-25 13:47:07 +0800

    chore(Storybook): refactor

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,53 +1,14 @@
 import { addParameters, configure } from '@storybook/react';
 
 import theme from './theme';
+import viewports from './viewports';
+
 addParameters({
   options: {
     theme,
   },
   viewport: {
-    viewports: {
-      xs: {
-        name: 'xs',
-        styles: {
-          width: '320px',
-          height: '100%',
-        },
-        type: 'mobile',
-      },
-      sm: {
-        name: 'sm',
-        styles: {
-          width: '480px',
-          height: '100%',
-        },
-        type: 'mobile',
-      },
-      md: {
-        name: 'md',
-        styles: {
-          width: '768px',
-          height: '100%',
-        },
-        type: 'tablet',
-      },
-      lg: {
-        name: 'lg',
-        styles: {
-          width: '992px',
-          height: '100%',
-        },
-        type: 'desktop',
-      },
-      xl: {
-        name: 'xl',
-        styles: {
-          width: '1200px',
-          height: '100%',
-        },
-        type: 'desktop',
-      },
-    },
+    viewports,
   },
 });
 
---
+++ .storybook/viewports.js
@@ -0,0 +1,43 @@
+export default {
+  xs: {
+    name: 'xs',
+    styles: {
+      width: '320px',
+      height: '100%',
+    },
+    type: 'mobile',
+  },
+  sm: {
+    name: 'sm',
+    styles: {
+      width: '480px',
+      height: '100%',
+    },
+    type: 'mobile',
+  },
+  md: {
+    name: 'md',
+    styles: {
+      width: '768px',
+      height: '100%',
+    },
+    type: 'tablet',
+  },
+  lg: {
+    name: 'lg',
+    styles: {
+      width: '992px',
+      height: '100%',
+    },
+    type: 'desktop',
+  },
+  xl: {
+    name: 'xl',
+    styles: {
+      width: '1200px',
+      height: '100%',
+    },
+    type: 'desktop',
+  },
+};
+
